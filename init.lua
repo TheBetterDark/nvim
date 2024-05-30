899973b737838a7bc0c635f5f0eed965c418ac7a
muabdi <>
2024-05-30 01:39:07 +0100

    Restructure Plugins

--- init.lua
+++ init.lua
@@ -1,6 +1,3 @@
-vim.cmd("set number")
-vim.g.mapleader = ' '
-
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
@@ -9,29 +6,10 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
     "clone",
     "--filter=blob:none",
     "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
+    "--branch=stable",
     lazypath,
   })
 end
 
 vim.opt.rtp:prepend(lazypath)
-
-local plugins = {
-	{ "AlexvZyl/nordic.nvim", lazy = false, priority = 1000, config = function() require("nordic").load() end },
-	{ "nvim-telescope/telescope.nvim", tag = "0.1.6", dependencies = { "nvim-lua/plenary.nvim" } },
-	{"nvim-treesitter/nvim-treesitter", build = ":TSUpdate"}
-}
-local opts = {}
-
-require("lazy").setup(plugins, opts)
-
-local builtin = require("telescope.builtin")
-vim.keymap.set('n' , "<C-p>", builtin.find_files, {})
-vim.keymap.set('n' , "<leader>fg", builtin.live_grep, {})
-
-local config = require("nvim-treesitter.configs")
-config.setup({
-	ensure_installed = {"lua", "c"},
-	highlight = { enable = true },
-	indent = { enable = true }
-})
+require("lazy").setup("plugins")
