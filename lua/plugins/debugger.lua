8476fb4134dfb63b5e8bcb0432357af950e9f30d
muabdi <>
2024-06-16 02:08:59 +0100

    Added custom signs to debugger

--- lua/plugins/debugger.lua
+++ lua/plugins/debugger.lua
@@ -26,23 +26,26 @@ return {
 			local dap, dapui = require("dap"), require("dapui")
 
 			require("dapui").setup()
+			require("dap.ext.vscode").load_launchjs(nil, {})
+
 			dap.listeners.before.attach.dapui_config = function()
 				dapui.open()
 			end
 			dap.listeners.before.launch.dapui_config = function()
 			  dapui.open()
 			end
-			dap.listeners.before.event_terminated.dapui_config = function()
-			  dapui.close()
-			end
-			dap.listeners.before.event_exited.dapui_config = function()
-			  dapui.close()
-			end
 
-			vim.keymap.set("n", "<Leader>dt", ":DapToggleBreakpoint<CR>")
-			vim.keymap.set("n", "<Leader>dc", ":DapContinue<CR>")
-			vim.keymap.set("n", "<Leader>dx", ":DapTerminate<CR>")
-			vim.keymap.set("n", "<Leader>do", ":DapStepOver<CR>")
+			vim.fn.sign_define('DapBreakpoint',{ text ='🛑', texthl ='', linehl ='', numhl =''})
+			vim.fn.sign_define('DapStopped',{ text ='▶️', texthl ='', linehl ='', numhl =''})
+			vim.fn.sign_define('DapBreakpointRejected', { text = '⚠️', texthl = 'LspDiagnosticsSignWarning', linehl = '', numhl = '' })
+
+			vim.keymap.set('n', '<F5>', dap.continue)
+			vim.keymap.set('n', '<F10>', dap.step_over)
+			vim.keymap.set('n', '<F11>', dap.step_into)
+			vim.keymap.set('n', '<F12>', dap.step_out)
+			vim.keymap.set('n', '<leader>b', dap.toggle_breakpoint)
+
+			vim.keymap.set('n', '<leader>do', dapui.toggle)
 		end
 	},
 }
